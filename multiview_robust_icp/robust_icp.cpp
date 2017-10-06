9d2b56ed74281b4bdd5b20109c6a5fdb11dc26b9
miaodx <miaodx@hotmail.com>
2017-10-06 19:31:05 +0800

    Add ICP

--- multiview_robust_icp/robust_icp.cpp
+++ multiview_robust_icp/robust_icp.cpp
@@ -38,6 +38,8 @@
 #include "opencv2/core/eigen.hpp"
 
 
+#include "icpPointToPoint.h"
+#include <icpPointToPlane.h>
 
 using namespace openMVG;
 using namespace openMVG::image;
@@ -59,11 +61,14 @@ cv::Point2d pixel2cam ( const cv::Point2d& p, const cv::Mat& K )
     );
 }
 
+void ICP_With_Point (const vector<cv::Point3f>& pts1, const vector<cv::Point3f>& pts2, vector<FLOAT>& R_vec, vector<FLOAT>& t_vec );
+
 int main( int argc, char **argv ) {
 
     string im1 = "1.jpg";
     string im2 = "2.jpg";
     string im1_depth = "1_depth.jpg";
+    string im2_depth = "2_depth.jpg";
     string output_json_file = "output.json";
     string camera_K_file = "H:/projects/SLAM/dataset/K.txt";
 
@@ -71,6 +76,7 @@ int main( int argc, char **argv ) {
     cmd.add ( make_option ( 'a', im1, "image1" ) );
     cmd.add ( make_option ( 'b', im2, "image2" ) );
     cmd.add ( make_option ( 'l', im1_depth, "image1_depth" ) );
+    cmd.add ( make_option ( 'r', im2_depth, "image2_depth" ) );
     cmd.add ( make_option ( 'K', camera_K_file, "camera_K_file" ) );
     cmd.add ( make_option ( 'o', output_json_file, "output_json file" ) );
 
@@ -85,6 +91,7 @@ int main( int argc, char **argv ) {
             << "[-a|--image1 - the file name of image1, absolute path, eg. H:/dataset/1.jpg]\n"
             << "[-b|--image2 - the name of image2]\n"
             << "[-l|--image1_depth - the name of image1 depth]\n"
+            << "[-r|--image2_depth - the name of image2 depth]\n"
             << "[-K|--camera_K_file - the file stores K values]\n"
             << "[-o|--output_json_file - json file for the R,t]\n"
             << std::endl;
@@ -187,7 +194,7 @@ int main( int argc, char **argv ) {
     );
   }
 
-  // Homography geometry filtering of putative matches
+
   {
     //A. get back interest point and send it to the robust estimation framework
     Mat xL(2, vec_PutativeMatches.size());
@@ -211,18 +218,21 @@ int main( int argc, char **argv ) {
 
     // 建立3D点
     cv::Mat d1 = cv::imread ( im1_depth, CV_LOAD_IMAGE_UNCHANGED );       // 深度图为16位无符号数，单通道图像
-    vector<cv::Point3f> pts_3d;
-    vector<cv::Point2f> pts_2d;
+    cv::Mat d2 = cv::imread ( im2_depth, CV_LOAD_IMAGE_UNCHANGED );
+    vector<cv::Point3f> pts_3d_1, pts_3d_2;
     /*
-    for ( DMatch m : matches )
+    for ( DMatch m:matches )
     {
-        ushort d = d1.ptr<unsigned short> ( int ( keypoints_1[m.queryIdx].pt.y ) )[int ( keypoints_1[m.queryIdx].pt.x )];
-        if ( d == 0 )   // bad depth
+        ushort d1 = depth1.ptr<unsigned short> ( int ( keypoints_1[m.queryIdx].pt.y ) ) [ int ( keypoints_1[m.queryIdx].pt.x ) ];
+        ushort d2 = depth2.ptr<unsigned short> ( int ( keypoints_2[m.trainIdx].pt.y ) ) [ int ( keypoints_2[m.trainIdx].pt.x ) ];
+        if ( d1==0 || d2==0 )   // bad depth
             continue;
-        float dd = d / 1000.0;
         Point2d p1 = pixel2cam ( keypoints_1[m.queryIdx].pt, K );
-        pts_3d.push_back ( Point3f ( p1.x*dd, p1.y*dd, dd ) );
-        pts_2d.push_back ( keypoints_2[m.trainIdx].pt );
+        Point2d p2 = pixel2cam ( keypoints_2[m.trainIdx].pt, K );
+        float dd1 = float ( d1 ) /1000.0;
+        float dd2 = float ( d2 ) /1000.0;
+        pts1.push_back ( Point3f ( p1.x*dd1, p1.y*dd1, dd1 ) );
+        pts2.push_back ( Point3f ( p2.x*dd2, p2.y*dd2, dd2 ) );
     }
     */
 
@@ -237,27 +247,25 @@ int main( int argc, char **argv ) {
         cv::Point2d kp1 ( xL.col ( k ).x (), xL.col ( k ).y () );
         cv::Point2d kp2 ( xR.col ( k ).x (), xR.col ( k ).y () );
 
-        ushort d = d1.ptr<unsigned short> ( int ( kp1.y ) )[int ( kp1.x )];
-        if ( d == 0 )   // bad depth
+        ushort d_1 = d1.ptr<unsigned short> ( int ( kp1.y ) )[int ( kp1.x )];
+        ushort d_2 = d2.ptr<unsigned short> ( int ( kp2.y ) )[int ( kp2.x )];
+        if ( d_1 == 0 || d_2 == 0 )   // bad depth
             continue;
-        float dd = d / 1000.0;
+        float dd_1 = d_1 / 1000.0;
+        float dd_2 = d_2 / 1000.0;
 
         cv::Point2d p1 = pixel2cam ( kp1, K_OCV );
-        pts_3d.push_back ( cv::Point3f ( p1.x*dd, p1.y*dd, dd ) );
-        pts_2d.push_back ( kp2 );
+        cv::Point2d p2 = pixel2cam ( kp2, K_OCV );
+        pts_3d_1.push_back ( cv::Point3f ( p1.x*dd_1, p1.y*dd_1, dd_1 ) );
+        pts_3d_2.push_back ( cv::Point3f ( p2.x*dd_2, p2.y*dd_2, dd_2 ) );        
     }
 
 
-    cv::Mat r, t;
-    //solvePnP ( pts_3d, pts_2d, K, Mat(), r, t, false ); // 调用OpenCV 的 PnP 求解，可选择EPNP，DLS等方法
-    solvePnPRansac ( pts_3d, pts_2d, K_OCV, cv::Mat (), r, t, false );
-    cv::Mat R;
-    cv::Rodrigues ( r, R ); // r为旋转向量形式，用Rodrigues公式转换为矩阵
-
-
-    cout << "All matched points num: " << pts_3d.size() << endl;
-    cout << "R=" << endl << R << endl;
-    cout << "t=" << endl << t << endl;
+    std::vector<double> rotation_vec ( 9 );
+    std::vector<double> translation_vec ( 3 );
+    //ICP_With_Point ( vector<cv::Point3f> pts1, vector<cv::Point3f> pts2 )
+    ICP_With_Point ( pts_3d_1, pts_3d_2, rotation_vec, translation_vec );
+    
 
 
     // Save R,t to file
@@ -265,11 +273,6 @@ int main( int argc, char **argv ) {
     j["im1"] = im1;
     j["im2"] = im2;
 
-    Mat R_eigen;
-    cv::cv2eigen ( R, R_eigen );
-    std::vector<double> rotation_vec ( 9 );
-    Eigen::Map<Eigen::MatrixXd> ( rotation_vec.data (), 3, 3 ) = R_eigen.transpose();
-    std::vector<double> translation_vec{ t.at<double>( 0 ) , t.at<double> ( 1 ), t.at<double> ( 2 ) };
     std::vector<double> K_vec ( 9 );
     Eigen::Map<Eigen::MatrixXd> ( K_vec.data (), 3, 3 ) = K.transpose ();
 
@@ -304,4 +307,76 @@ bool readIntrinsic ( const std::string & fileName, Mat3 & K )
         return false;
     }
     return true;
+}
+
+void ICP_With_Point ( const vector<cv::Point3f>& pts1, const vector<cv::Point3f>& pts2, vector<FLOAT>& R_vec, vector<FLOAT>& t_vec )
+{
+    int32_t dim = 3;
+    //int32_t num = 10000;
+    int32_t num = pts1.size ();
+
+    // allocate model and template memory
+    double* M = (double*)calloc ( 3 * num, sizeof ( double ) );
+    double* T = (double*)calloc ( 3 * num, sizeof ( double ) );
+
+    // set model and template points
+    cout << endl << "Creating model with " << num << " points ..." << endl;
+
+    for ( int32_t k = 0; k < pts1.size (); k++ )
+    {
+        cv::Point3f p1 = pts1[k];
+        cv::Point3f p2 = pts2[k];
+        //cout << "p1:" << p1 << endl;
+
+        M[k * 3 + 0] = p1.x;
+        M[k * 3 + 1] = p1.y;
+        M[k * 3 + 2] = p1.z;
+
+        T[k * 3 + 0] = p2.x;
+        T[k * 3 + 1] = p2.y;
+        T[k * 3 + 2] = p2.z;
+    }
+
+    cout << "assign values done" << endl;
+
+
+    // start with identity as initial transformation
+    // in practice you might want to use some kind of prediction here
+    Matrix R = Matrix::eye ( 3 );
+    Matrix t ( 3, 1 );
+
+    // run point-to-plane ICP (-1 = no outlier threshold)
+    cout << endl << "Running ICP" << endl;
+    IcpPointToPoint icp ( M, num, dim );
+    //IcpPointToPlane icp ( M, num, dim );
+    icp.fit ( T, num, R, t, -1 );
+
+    // free memory
+    free ( M );
+    free ( T );
+
+    // results
+    cout << endl << "Transformation results:" << endl;
+    cout << "R:" << endl << R << endl << endl;
+    cout << "t:" << endl << t << endl << endl;
+
+    
+    R.getData ( R_vec.data () );
+    t.getData ( t_vec.data () );
+    //cout << "R_vec:" << R_vec.data() << endl;
+    cout << "R_vec:" << endl;
+    for(auto i :R_vec )
+    {
+        cout << " " << i;
+    }
+    cout << endl;
+    
+    cout << "t_vec:" << endl;
+    for ( auto i : t_vec )
+    {
+        cout << " " << i;
+    }
+    cout << endl;
+
+
 }
